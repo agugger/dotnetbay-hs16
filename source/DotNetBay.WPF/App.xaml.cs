a42310d6d327c8865a4db88939bbacb55c76c024
Daellen <david.aellen@students.fhnw.ch>
2016-10-05 13:00:59 +0200

    klini appassig weiss ned genau ob da so stimmt vor DemoData

--- source/DotNetBay.WPF/App.xaml.cs
+++ source/DotNetBay.WPF/App.xaml.cs
@@ -5,6 +5,10 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using DotNetBay.Core;
+using DotNetBay.Core.Execution;
+using DotNetBay.Interfaces;
+using DotNetBay.Data.Provider.FileStorage;
 
 namespace DotNetBay.WPF
 {
@@ -13,5 +17,14 @@ namespace DotNetBay.WPF
     /// </summary>
     public partial class App : Application
     {
+        public readonly IMainRepository MainRepository = new FileSystemMainRepository("Filename"); //Name der Datei zum Speichern der Daten
+
+        public IAuctionRunner AuctionRunner;
+
+        public App()
+        {
+            AuctionRunner = new AuctionRunner(MainRepository);
+            AuctionRunner.Start();
+        }
     }
 }
