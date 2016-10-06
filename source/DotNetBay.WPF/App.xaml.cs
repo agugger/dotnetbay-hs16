9d8f1aad451d8e749db351670e9d6e96d55617a9
Daellen <david.aellen@students.fhnw.ch>
2016-10-06 13:06:29 +0200

    soht bis vor Ufgob 2 gmacht si

--- source/DotNetBay.WPF/App.xaml.cs
+++ source/DotNetBay.WPF/App.xaml.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Security.Authentication.ExtendedProtection.Configuration;
 using System.Threading.Tasks;
 using System.Windows;
 using DotNetBay.Core;
 using DotNetBay.Core.Execution;
+using DotNetBay.Data.Entity;
 using DotNetBay.Interfaces;
 using DotNetBay.Data.Provider.FileStorage;
 
@@ -17,14 +19,36 @@ namespace DotNetBay.WPF
     /// </summary>
     public partial class App : Application
     {
+        
+
         public readonly IMainRepository MainRepository = new FileSystemMainRepository("Filename"); //Name der Datei zum Speichern der Daten
 
         public IAuctionRunner AuctionRunner;
 
         public App()
         {
+
             AuctionRunner = new AuctionRunner(MainRepository);
             AuctionRunner.Start();
+
+            var memberService = new SimpleMemberService(this.MainRepository);
+            var service = new AuctionService(this.MainRepository, memberService);
+            if (!service.GetAll().Any())
+            {
+                var me = memberService.GetCurrentMember();
+
+                service.Save(new Auction
+                {
+                    Title = "My First Auction",
+                    StartDateTimeUtc = DateTime.UtcNow.AddSeconds(10),
+                    EndDateTimeUtc = DateTime.UtcNow.AddDays(14),
+                    StartPrice = 72,
+                    Seller = me
+                });
+            }
+
+
+
         }
     }
 }
